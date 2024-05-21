956be5fbd2901e528860ace51a9f5526f0cd9329
Juan Petrich <petrak.893@gmail.com>
2024-05-21 13:42:19 -0300

    Inicio y cerrado de sesion + natvar

--- project/core/views.py
+++ project/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
+from .form import CustomAuthenticationForm
 # Create your views here.
 from . import models
 
 def home(request):
     return render(request, "core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
