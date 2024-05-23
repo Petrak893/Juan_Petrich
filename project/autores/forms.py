f29de560778f940587914b69c76afbe853f2d38e
Juan Petrich <petrak.893@gmail.com>
2024-05-23 11:18:49 -0300

    Arreglado del modelo y formulario de autores

--- project/autores/forms.py
+++ project/autores/forms.py
@@ -1,13 +1,25 @@
 from django import forms
 from .models import Autor
 
+
 class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
-        fields = "__all__"
+        fields = ['nombre',
+                  'es_dibujante',
+                  'es_escritor',
+                  'fecha_nacimiento',
+                  'biografia',
+                  'imagen']
         widgets = {
-            "nombre": forms.TextInput(attrs={"class": "form-control"}),
-            "numero del nivel": forms.TextInput(attrs={"class": "form-control"}),
-            "descripcion": forms.TextInput(attrs={"class": "form-control"}),
-            
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'es_dibujante': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'es_escritor': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'fecha_nacimiento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'biografia': forms.Textarea(attrs={'class': 'form-control'}),
+            'imagen': forms.FileInput(attrs={'class': 'form-control-file'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['fecha_nacimiento'].widget.attrs['class'] = 'form-control mt-2'
--- project/autores/models.py
+++ project/autores/models.py
@@ -4,9 +4,9 @@ class Autor(models.Model):
     nombre = models.CharField(max_length=100)
     es_dibujante = models.BooleanField(default=False)
     es_escritor = models.BooleanField(default=False)
-    edad = models.IntegerField()
     fecha_nacimiento = models.DateField()
     biografia = models.TextField()
+    imagen = models.ImageField(upload_to='imagenes_personajes/', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
