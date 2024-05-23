4cfe784b3a795b5a59551dd31f3accbd3db7710c
Juan Petrich <petrak.893@gmail.com>
2024-05-23 12:02:53 -0300

    Mejora de apriencia del crud de autores

--- project/autores/urls.py
+++ project/autores/urls.py
@@ -5,7 +5,7 @@ app_name = "autores"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('autores/list', views.AutorListView.as_view(), name='autores_list'),
+    path('autores/list/', views.AutorListView.as_view(), name='autores_list'),
     path('autores/detail/<int:pk>', views.AutorDetailView.as_view(), name='autores_detail'),
     path('autores/create/', views.AutorCreateView.as_view(), name='autores_create'),
     path('autores/update/<int:pk>', views.AutorUpdateView.as_view(), name='autores_update'),
